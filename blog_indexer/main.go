5d111df5b845855ba8a0ab411d776ce96d3ea57a
Ian McGaunn <ianmcgaunn@pressmail.ch>
2018-07-08 09:10:03 -0400

    Add parsing of blog post metadata and index upkeep logic to indexer!

--- blog_indexer/main.go
+++ blog_indexer/main.go
@@ -2,18 +2,162 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"imcgbackend/blog/index"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-// this is returned by
-func updateBlogIndex(ctx context.Context, s3Event events.S3Event) {
-	for _, record := range s3Event.Records {
-		s3 := record.S3
-		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+func printEventDetails(eventRecord events.S3EventRecord) {
+	log.Printf("--\n")
+	log.Printf("event name: [%s]\n", eventRecord.EventName)
+	log.Printf("event source: [%s]\n", eventRecord.EventSource)
+	log.Printf("event time: [%s]\n", eventRecord.EventTime)
+	log.Printf("bucket, key: [%s, %s]\n", eventRecord.S3.Bucket.Name, eventRecord.S3.Object.Key)
+	log.Printf("--\n")
+}
+
+// TODO: figure out if it is practical to share this code with 'index' module
+func BuildS3DownloadManager() *s3manager.Downloader {
+	sess := session.Must(session.NewSession())
+	svc := s3manager.NewDownloader(sess)
+	return svc
+}
+
+func extractPostHeaderLines(postLines []string) ([]string, error) {
+	// fetch metadata from the beginning of the post
+	// scan until you see prelude's bottom marker.
+	lastHeaderRowIdx := -1
+	for pos, str := range postLines {
+		if str[:3] == ">>>" {
+			lastHeaderRowIdx = pos
+			log.Println(str)
+			break
+		}
 	}
+	if lastHeaderRowIdx > 0 {
+		log.Printf("the header, yo: \n")
+		headerLines := postLines[:lastHeaderRowIdx]
+		for line := range headerLines {
+			log.Println(headerLines[line])
+		}
+		return headerLines, nil
+	}
+	return nil, errors.New("failed to extract header")
+}
+
+func parseHeaderLines(lines []string) map[string]string {
+	metaDataMap := make(map[string]string)
+	for line := range lines {
+		newMap, err := parseHeaderLine(lines[line])
+		if err != nil {
+			panic("encountered bad header line bye bye")
+		}
+		for newKey, val := range newMap {
+			metaDataMap[newKey] = val
+		}
+	}
+	return metaDataMap
+}
+
+func parseHeaderLine(line string) (map[string]string, error) {
+	components := strings.Split(line, ":")
+	if len(components) < 2 {
+		return nil, errors.New("invalid header line")
+	}
+	metaData := map[string]string{
+		components[0]: components[1],
+	}
+	return metaData, nil
+}
+
+func downloadIndexIfNecessary() *sql.DB {
+	// TODO: more conditional love
+	dbBytes, err := index.GetIndexDbFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile("/tmp/index.sqlite", dbBytes, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := sql.Open("sqlite3", "file:/tmp/index.sqlite?_loc=auto")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conn
+}
+
+func processIncomingPost(bucket string, key string, eventTime time.Time, downloader *s3manager.Downloader) {
+	buffer := aws.NewWriteAtBuffer(make([]byte, 1024))
+	bytesRead, err := downloader.Download(buffer, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		panic(err)
+	}
+	db := downloadIndexIfNecessary()
+	defer db.Close()
+	postS3Uri := fmt.Sprintf("s3://%s/%s", bucket, key)
+	ie, err := index.GetIndexEntryByS3Location(postS3Uri, db)
+	if err != nil {
+		log.Printf("there's already an index entry for this post [%s] so i'm ignoring it\n", postS3Uri)
+		log.Print("the new index entry has this info: ")
+		log.Print(ie)
+	}
+
+	postContent := string(buffer.Bytes()[:bytesRead])
+	postLines := strings.Split(postContent, "\n")
+	headerLines, err := extractPostHeaderLines(postLines)
+	if err != nil {
+		log.Printf("there doesn't seem to be a real header. too bad :(")
+		return
+	}
+	postMetaData := parseHeaderLines(headerLines)
+
+	newindexEntry := index.BlogIndexEntry{PostS3Loc: postS3Uri,
+		PostMetaS3Loc: "nothinyet.metadataisinline",
+		CreatedTime:   eventTime}
+	log.Print("adding new index entry to the database")
+	res, err := index.AddIndexEntry(newindexEntry, db)
+	if err != nil {
+		log.Printf("failed to add index entry to the database")
+		log.Fatal(err)
+	}
+	log.Print("added to database: ")
+	log.Print(newindexEntry)
+	log.Print(res)
+	log.Print(postMetaData)
+}
+
+func updateBlogIndex(ctx context.Context, s3Event events.S3Event) {
+	log.Printf("we are about to update the index! wish me luck")
+	for _, record := range s3Event.Records {
+		printEventDetails(record)
+		// bail out if this isn't a putObject event
+		// no test events allowed!
+		if record.EventName != "ObjectCreated:Put" {
+			return
+		}
+
+		evtTime := record.EventTime
+		s3 := record.S3
+		downloader := BuildS3DownloadManager()
+		processIncomingPost(s3.Bucket.Name, s3.Object.Key, evtTime, downloader)
+	}
+	log.Printf("successfully updated index :) :) ")
 }
 
 func main() {
