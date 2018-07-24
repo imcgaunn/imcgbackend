670ed73d605d6acc204aba7dd3b9a19ec8484715
Ian McGaunn <ianmcgaunn@pressmail.ch>
2018-07-24 00:24:03 -0400

    Fix CORS. Will refactor the response functions later

--- aws/apigateway/util.go
+++ aws/apigateway/util.go
@@ -5,11 +5,19 @@ import "github.com/aws/aws-lambda-go/events"
 func BuildSuccessResponse(body string) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{
 		Body:       body,
-		StatusCode: 200}
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin" : "*",
+			"Content-Type": "application/edn"},
+	}
 }
 
 func BuildFailureResponse(body string) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{
 		Body:       body,
-		StatusCode: 500}
+		StatusCode: 500,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/edn"},
+	}
 }
